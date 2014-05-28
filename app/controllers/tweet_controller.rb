90a358d12694a66667110c84df43bf8214e3ccd2
menezes <marcelo.menezes@prodec.com.br>
2014-05-28 17:48:50 -0300

    Twitter_test

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,32 @@
+class TweetController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @tweet_item = Tweet.all
+  end
+
+  def create
+    @tweet_item = Tweet.new(sec_params)
+
+    @tweet_item.save
+    redirect_to new_tweet_path
+  end
+
+  def destroy
+    @tweet_item = Tweet.find(params[:id])
+
+    @tweet_item.destroy
+    redirect_to new_tweet_path
+  end
+
+  private
+    def sec_params
+      params.require(:tweet).permit(:msg)
+    end
+
+end
