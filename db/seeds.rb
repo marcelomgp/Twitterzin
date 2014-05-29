6a6027528499e68d8d5df2d8f339d0f35aaf1ee5
menezes <marcelo.menezes@prodec.com.br>
2014-05-29 17:43:13 -0300

    update on 29 Mai

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tweet.create([{msg: 'Seed asad'}, {msg: 'Seed asad'}, {msg: 'Seed asad'}, {msg: 'Seed asad'}, {msg: 'Seed asad'}])
